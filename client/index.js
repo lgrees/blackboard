086e0f129a11e6fce7c1d829c8d117f4b5de6db6
lgrees <lukasgrees@gmail.com>
2018-11-09 15:12:41 -0600

    adds polynomial regression

--- client/index.js
+++ client/index.js
@@ -1,6 +1,16 @@
-import { loss, optimizer, predict } from '../models/regression';
+// import { loss, optimizer, predict } from '../models/linear-regression';
+import { loss, optimizer, predict } from '../models/polynomial-regression';
 import p5 from 'p5';
 import * as tf from '@tensorflow/tfjs';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+);
 
 let xVals = [];
 let yVals = [];
@@ -30,15 +40,44 @@ let s = sk => {
         sk.point(px, py);
       }
 
-      let xs = [0, 1];
-      let ys = predict(xs);
-      let x1 = sk.map(xs[0], 0, 1, 0, 400);
-      let x2 = sk.map(xs[1], 0, 1, 0, 400);
-      let lineY = ys.dataSync();
-      let y1 = sk.map(lineY[0], 0, 1, 400, 0);
-      let y2 = sk.map(lineY[1], 0, 1, 400, 0);
+      // ----Linear regression
+      // let xs = [0, 1];
+      // let ys = predict(xs);
+      // let x1 = sk.map(xs[0], 0, 1, 0, 400);
+      // let x2 = sk.map(xs[1], 0, 1, 0, 400);
+      // let lineY = ys.dataSync();
+      // let y1 = sk.map(lineY[0], 0, 1, 400, 0);
+      // let y2 = sk.map(lineY[1], 0, 1, 400, 0);
+      // sk.line(x1, y1, x2, y2);
 
-      sk.line(x1, y1, x2, y2);
+      // ----Polynomial regression
+      let xs = [];
+      for (let i = 0; i <= 1; i += 0.05) {
+        xs.push(i);
+      }
+      let yTense = predict(xs);
+      let ys = yTense.dataSync();
+
+      sk.beginShape();
+      sk.noFill();
+
+      sk.stroke(255);
+      sk.strokeWeight(2);
+
+      for (let i = 0; i < xs.length; i++) {
+        let x = sk.map(xs[i], 0, 1, 0, 400);
+        let y = sk.map(ys[i], 0, 1, 400, 0);
+        sk.vertex(x, y);
+      }
+
+      sk.endShape();
+
+      // let x1 = sk.map(xs[0], 0, 1, 0, 400);
+      // let x2 = sk.map(xs[1], 0, 1, 0, 400);
+      // let lineY = ys.dataSync();
+      // let y1 = sk.map(lineY[0], 0, 1, 400, 0);
+      // let y2 = sk.map(lineY[1], 0, 1, 400, 0);
+
       console.log(tf.memory());
     });
   };
