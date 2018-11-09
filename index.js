2237db0861003a345aad858888b0341376f2591c
lgrees <lukasgrees@gmail.com>
2018-11-09 13:22:40 -0600

    adds memory mgmt

--- index.js
+++ index.js
@@ -13,31 +13,34 @@ let s = sk => {
 
   sk.draw = () => {
     sk.background(80);
-    if (xVals.length > 0) {
-      const ys = tf.tensor1d(yVals);
-      optimizer.minimize(() => loss(predict(xVals), ys));
-    }
 
-    //designating how points should look
-    sk.stroke(255);
-    sk.strokeWeight(8);
+    tf.tidy(() => {
+      if (xVals.length > 0) {
+        const ys = tf.tensor1d(yVals);
+        optimizer.minimize(() => loss(predict(xVals), ys));
+      }
 
-    for (let i = 0; i < xVals.length; i++) {
-      let px = sk.map(xVals[i], 0, 1, 0, 400);
-      let py = sk.map(yVals[i], 0, 1, 400, 0);
-      sk.point(px, py);
-    }
+      //designating how points should look
+      sk.stroke(255);
+      sk.strokeWeight(8);
 
-    let xs = [0, 1];
-    let ys = predict(xs);
-    let x1 = sk.map(xs[0], 0, 1, 0, 400);
-    let x2 = sk.map(xs[1], 0, 1, 0, 400);
-    let lineY = ys.dataSync();
-    let y1 = sk.map(lineY[0], 0, 1, 400, 0);
-    let y2 = sk.map(lineY[1], 0, 1, 400, 0);
+      for (let i = 0; i < xVals.length; i++) {
+        let px = sk.map(xVals[i], 0, 1, 0, 400);
+        let py = sk.map(yVals[i], 0, 1, 400, 0);
+        sk.point(px, py);
+      }
 
-    sk.line(x1, y1, x2, y2);
-    tf.dispose(ys);
+      let xs = [0, 1];
+      let ys = predict(xs);
+      let x1 = sk.map(xs[0], 0, 1, 0, 400);
+      let x2 = sk.map(xs[1], 0, 1, 0, 400);
+      let lineY = ys.dataSync();
+      let y1 = sk.map(lineY[0], 0, 1, 400, 0);
+      let y2 = sk.map(lineY[1], 0, 1, 400, 0);
+
+      sk.line(x1, y1, x2, y2);
+      console.log(tf.memory());
+    });
   };
   sk.mousePressed = () => {
     let x = sk.map(sk.mouseX, 0, 400, 0, 1);
